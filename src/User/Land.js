d0cc2f7360d5c339493515b57de61a686314e47f
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-21 23:30:45 +0100

    working on the register page functionality

--- src/User/Land.js
+++ src/User/Land.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Land = () => {
   return (
-    <div>Land</div>
+    <div>
+   <Link to='/register'>   <button class="bg-blue-500 text-center mx-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Click me
+      </button></Link>
+
+    </div>
   )
 }
 
