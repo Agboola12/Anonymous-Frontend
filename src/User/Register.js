c432c98f257edefcb247a534fc3e503f9e33acff
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-21 10:40:13 +0100

    working on the register page

--- src/User/Register.js
+++ src/User/Register.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import logo from './logo.png'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='bg-secondary h-auto w-full'>
+      <div className='bg-primary'>
+      <div className=" w-full h-fit rounded-lg mt-5 overflow-hidden  shadow-xl">
+         <div className="  text-green-500 mt-4 text-center">
+             
+           <div className="font-bold text-2xl mb-2">You have successfully <br/> logged in!! </div>
+         </div>
+      </div>
+        {/* <img src={logo} alt='logo' className='' /> */}
+      </div>
+
+    </div>
   )
 }
 
