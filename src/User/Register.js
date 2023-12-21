d9521716b62518afdcbaf9ae3e21d36b2eabd9fd
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-21 12:51:42 +0100

    working on the register page

--- src/User/Register.js
+++ src/User/Register.js
@@ -7,15 +7,27 @@ const Register = () => {
     <div className=' py-5 h-auto w-full bg-gradient-to-r from-secondary to-primary'>
       <div className="w-[40%] py-4 mx-auto  bg-primary h-screen text-white rounded overflow-hidden shadow-lg">
         <div className='mt-5'>
-          <img className="rounded  mx-auto w-40" src={logo} alt="Card image" />
+          <img className="rounded  mx-auto w-28" src={logo} alt="Card image" />
         </div>
         <div className="px-6 py-4">
           <form>
-            <div class={style.group}>
-              <input required="" type="text" class={style.input}/>
-                <span class="highlight"></span>
-                <span class="bar"></span>
-                <label className={style.label}>Name</label>
+            <div id={style.group} className='mt-[3em]'>
+              <input required="" type="text" placeholder='Enter Your Username' className={style.input}/>
+                <span className={style.highlight}></span>
+                <span className={style.bar}></span>
+            </div>
+            <div id={style.group} className='mt-[3em]'>
+              <input required="" type="email" placeholder='Enter Your email' className={style.input}/>
+                <span className={style.highlight}></span>
+                <span className={style.bar}></span>
+            </div>
+            <div id={style.group} className='mt-[3em]'>
+              <input required="" type="password" placeholder='Enter Your Password' className={style.input}/>
+                <span className={style.highlight}></span>
+                <span className={style.bar}></span>
+            </div>
+            <div className='mt-12 text-center'>
+              <button className='bg-gradient-to-r from-secondary to-primary w-60 h-14 text-white font-bold py-2 px-4 rounded text-center'> Register</button>
             </div>
           </form>
         </div>
