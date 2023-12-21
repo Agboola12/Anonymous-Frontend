20ec46b724b70824250a3cd2fdfc1674f09c946f
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-21 21:45:43 +0100

    working on the message page

--- src/User/Message.js
+++ src/User/Message.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import style from '../style/register.module.css'
 
 const Message = () => {
   return (
@@ -7,17 +8,16 @@ const Message = () => {
         <div className="px-6 py-4">
             <h1 className='text-center font-extrabold '>Say Something...</h1>
           <form>
-            <div id={style.group} className='mt-[3em]'>
-              <input required="" type="password" placeholder='Enter Your Password' className={style.input} />
+            <div id={style.group}>
+              <textarea required="" type="text" placeholder='Leave a message here ' className={style.input} />
               <span className={style.highlight}></span>
               <span className={style.bar}></span>
+              <label className={style.label}>Say Something About Me</label>
             </div>
             <div className='mt-12 text-center'>
-              <button className='bg-gradient-to-r from-secondary to-primary w-60 h-14 text-white font-bold py-2 px-4 rounded text-center'>
-                Login
-              </button><br />
-              <Link to="/register" className={style.login}>Don't have an account?Register</Link><br /><br/>
-              <small className='text-light '>By using this service, you agree to our Privacy Policy, Terms of Service and any related policies. (Check Disclaimer)</small>
+              <button className='bg-gradient-to-r from-secondary to-primary w-80 h-14 rounded-full text-white font-bold py-2 px-4 rounded text-center'>
+                Send Message
+              </button>
             </div>
           </form>
         </div>
