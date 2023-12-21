20a1287baa1383b35b99657424147e980e5152d2
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-21 12:14:37 +0100

    working on the register page

--- src/User/Register.js
+++ src/User/Register.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import logo from './logo.png'
+import style from '../style/register.module.css'
 
 const Register = () => {
   return (
     <div className=' py-5 h-auto w-full bg-gradient-to-r from-secondary to-primary'>
-      <div className="w-1/3 py-4 mx-auto  bg-primary h-screen text-white rounded overflow-hidden shadow-lg">
+      <div className="w-[40%] py-4 mx-auto  bg-primary h-screen text-white rounded overflow-hidden shadow-lg">
         <div className='mt-5'>
           <img className="rounded  mx-auto w-40" src={logo} alt="Card image" />
         </div>
         <div className="px-6 py-4">
-            <form>
-              </form>         
-        </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Tag1</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#Tag2</span>
+          <form>
+            <div class={style.group}>
+              <input required="" type="text" class={style.input}/>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label className={style.label}>Name</label>
+            </div>
+          </form>
         </div>
       </div>
 
