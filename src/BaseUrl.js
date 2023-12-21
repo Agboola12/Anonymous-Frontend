0312089a1d75e9bacedc7ffeb917726e496b4a9c
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-21 23:15:47 +0100

    working on the register page functionality

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,13 +1,4 @@
-import { io } from "socket.io-client";
 
-let baseUrl ;
-let socket;
-    if (process.env.NODE_ENV === "production"){
-        // 
-    }else{
-       baseUrl= "http://localhost:5000/" ;
-       socket = io("http://localhost:5000/");
-    }
+       let BaseUrl ="http://localhost:6000/";
 
-    export {socket};
-    export default baseUrl;
+export default BaseUrl;
--- src/User/Register.js
+++ src/User/Register.js
@@ -2,63 +2,81 @@ import React, { useState } from 'react'
 import logo from './logo.png'
 import style from '../style/register.module.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import BaseUrl from '../BaseUrl'
+import { useNavigate } from 'react-router-dom';
+
 
 const Register = () => {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false)
-  const [user, setUser]=useState({
-    username:"",
-    email:"",
-    password:""
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: ""
   })
 
-  const inputChange =(e)=>{
+  const inputChange = (e) => {
     const name = e.target.name
     const value = e.target.value
-    setUser({...user, [name]:value})
-
-  }  
-
-  const handleSubmit =()=>{
+    setUser({ ...user, [name]: value })
 
   }
 
-return (
-  <div className=' py-5 h-auto w-full bg-gradient-to-r from-secondary to-primary'>
-    <div className="w-[35%] py-4 mx-auto  bg-primary h-screen text-white rounded-2xl overflow-hidden shadow-lg">
-      <div className='mt-5'>
-        <img className="rounded  mx-auto w-28" src={logo} alt="wow" />
-      </div>
-      <div className="px-6 py-4">
-        <h1 className={style.head}>Register</h1>
-        <form method="post" action="" onSubmit={handleSubmit}>
-          <div id={style.group} className='mt-[1em]'>
-            <input required type="text" placeholder='Enter Your Username' className={style.input} name='username' value={user.username} onChange={inputChange}/>
+  const handleSubmit = () => {
+    setIsLoading(true);
+    axios.post(BaseUrl + "register", user)
+      .then((res) => {
+        if (res.data.status) {
+          console.log(res)
+          navigate('/login');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      }).finally(() => {
+        isLoading(false);
+      })
+
+  }
+
+  return (
+    <div className=' py-5 h-auto w-full bg-gradient-to-r from-secondary to-primary'>
+      <div className="w-[35%] py-4 mx-auto  bg-primary h-screen text-white rounded-2xl overflow-hidden shadow-lg">
+        <div className='mt-5'>
+          <img className="rounded  mx-auto w-28" src={logo} alt="wow" />
+        </div>
+        <div className="px-6 py-4">
+          <h1 className={style.head}>Register</h1>
+          <form method="post" action="" onSubmit={handleSubmit}>
+            <div id={style.group} className='mt-[1em]'>
+              <input required type="text" placeholder='Enter Your Username' className={style.input} name='username' value={user.username} onChange={inputChange} />
               <span className={style.highlight}></span>
               <span className={style.bar}></span>
-          </div>
-          <div id={style.group} className='mt-[2em]'>
-            <input required="" type="email" placeholder='Enter Your email' className={style.input} name='email' value={user.email} onChange={inputChange}/>
+            </div>
+            <div id={style.group} className='mt-[2em]'>
+              <input required="" type="email" placeholder='Enter Your email' className={style.input} name='email' value={user.email} onChange={inputChange} />
               <span className={style.highlight}></span>
               <span className={style.bar}></span>
-          </div>
-          <div id={style.group} className='mt-[2em]'>
-            <input required="" type="password" placeholder='Enter Your Password' className={style.input} name='password' value={user.password} onChange={inputChange}/>
+            </div>
+            <div id={style.group} className='mt-[2em]'>
+              <input required="" type="password" placeholder='Enter Your Password' className={style.input} name='password' value={user.password} onChange={inputChange} />
               <span className={style.highlight}></span>
               <span className={style.bar}></span>
-          </div>
-          <div className='mt-12 text-center'>
-            <button type='submit' disabled={isLoading} className='bg-gradient-to-r from-secondary to-primary w-60 h-14 text-white font-bold py-2 px-4 rounded text-center'>
-            {isLoading ? "Saving post..." : "Register"}
-           </button><br/>
-           
-           <Link to="/login" className={style.login}>Already an account? Login</Link><br/><br/>
-           <small className='text-light'>By using this service, you agree to our Privacy Policy, Terms of Service and any related policies. (Check Disclaimer)</small>
-          </div>
-        </form>
+            </div>
+            <div className='mt-12 text-center'>
+              <button type='submit' disabled={isLoading} className='bg-gradient-to-r from-secondary to-primary w-60 h-14 text-white font-bold py-2 px-4 rounded text-center'>
+                {isLoading ? "Saving post..." : "Register"}
+              </button><br />
+
+              <Link to="/login" className={style.login}>Already an account? Login</Link><br /><br />
+              <small className='text-light'>By using this service, you agree to our Privacy Policy, Terms of Service and any related policies. (Check Disclaimer)</small>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default Register
