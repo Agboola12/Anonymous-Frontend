84d6b24f75f825bb3e8fe4effdeb890cc8805598
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-22 10:39:09 +0100

    making the desing responsive

--- src/User/Message.js
+++ src/User/Message.js
@@ -1,39 +1,65 @@
 import React from 'react'
-import style from '../style/register.module.css'
 
 const Message = () => {
   return (
-    <div className=' py-5 h-[100vh] w-full bg-gradient-to-r from-secondary to-primary'>
-      <div className="w-[35%] py-4 mx-auto  bg-primary h-[90vh] text-white rounded-2xl overflow-hidden shadow-lg">
-        <div className="px-6 py-4">
-            <h1 className={style.say}>Say Something...</h1>
-          <form>
-            <div id={style.group} className='mt-14'>
-              {/* <textarea required="" type="text" placeholder='Leave a message here ' className={style.input} /> */}
-              {/* <textarea
-  type="text"
-  class="text-base p-2 pb-1 border-b border-purple-300 bg-transparent w-full mt-20 h-30vh focus:outline-none"
-  placeholder="Your Placeholder Text"
-/> */}
-<textarea
-  type="text"
-  className="text-base sm:text-lg md:text-xl lg:text-2xl bg-transparent border-b border-purple-300 w-full py-2 px-2 sm:px-4 lg:px-5 mt-5 h-1/3 sm:h-1/4 md:h-1/5 lg:h-1/3"
-  placeholder="Your text here"
-/>
-
-              <span className={style.highlight}></span>
-              <span className={style.bar}></span>
-              <label className={style.label}>Say Something About Me</label>
+    <div className='h-screen py-5 w-full bg-gradient-to-r from-secondary to-primary'>
+      <div className='w-full sm:w-2/3 md:w-1/1 lg:w-1/3 xl:w-1/3 mx-auto bg-primary text-white rounded-2xl overflow-hidden shadow-lg'>
+        <div className='mt-5'>
+          <img className='rounded mx-auto w-28' src={logo} alt='logo' />
+        </div>
+        <div className='px-6 py-4'>
+          <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold text-center text-white'>
+          Say Something...
+          </h1>
+          <form
+            method='post'
+            action=''
+            encType='multipart/form-data'
+            onSubmit={handleSubmit}
+          >
+            <div className='flex flex-col mt-9'>
+              <div className='relative mb-4'>
+                <textarea
+                  id='message'
+                  name='message'
+                  type='message'
+                  required
+                  className='w-full border-b border-primary-300 py-1 focus:border-b-2 focus:border-primary-700 transition-colors focus:outline-none peer bg-inherit'
+                />
+                <label
+                  htmlFor='message'
+                  className='absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700'
+                >
+                  Say Something about me
+                </label>
+              </div>
+              
             </div>
-            <div className='mt-12 text-center'>
-              <button className='bg-gradient-to-r from-secondary to-primary w-80 h-14 rounded-full text-white font-bold py-2 px-4 rounded text-center'>
-                Send Message
+            <div className='mt-8 text-center'>
+              <button
+                type='submit'
+                disabled={isLoading}
+                className='bg-gradient-to-r from-secondary to-primary w-full sm:w-2/3 md:w-1/2 lg:w-3/4 xl:w-1/3 h-14 text-white font-bold py-2 px-4 rounded text-center'
+              >
+                <b>{isLoading ? 'Sending...' : 'Send Message'}</b>
               </button>
+              <br />
+              <Link
+                to='/register'
+                className='text-lightgrey hover:text-blue-500'
+              >
+                Don't have an account? Register
+              </Link>
+              <br />
+              <br />
+              <small className='text-light'>
+                By using this service, you agree to our Privacy Policy, Terms
+                of Service and any related policies. (Check Disclaimer)
+              </small>
             </div>
           </form>
         </div>
       </div>
-
     </div>
   )
 }
